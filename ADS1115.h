d859c629430d5d5153e435565ec3ae889acc9b35
Andy Wickert <andy@northernwidget.com>
2019-02-13 19:24:10 -0600

    Create ADS1115.h
    Basic template

---
+++ ADS1115.h
@@ -0,0 +1,20 @@
+#ifndef ADS1115_h
+#define ADS1115_h
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+class ADS1115
+{
+  public:
+
+  private:
+};
+
+#endif
